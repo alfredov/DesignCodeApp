c4e5d068b671e9f8ac196c4fb5e1de0eafcff69d
Alfredo V <alfredo.villagomez.m@gmail.com>
2018-10-24 18:46:49 -0400

    Initial Commit

---
+++ DesignCodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignCodeApp
+//
+//  Created by Alfredo Villagomez on 10/24/18.
+//  Copyright © 2018 Alfredo Villagomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
