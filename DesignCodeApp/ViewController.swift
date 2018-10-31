1ee3217d77e91f1da800c99c3077bcd3b682524d
Alfredo V <alfredo.villagomez.m@gmail.com>
2018-10-31 12:27:15 -0400

    Playing video sample

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deviceImageView: UIImageView!
+    @IBOutlet weak var playVisualView: UIVisualEffectView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.titleLabel.alpha = 0
+        deviceImageView.alpha = 0
+        playVisualView.alpha = 0
+        
+        UIView.animate(withDuration: 1) {
+            self.titleLabel.alpha = 1
+            self.deviceImageView.alpha = 1
+            self.playVisualView.alpha = 1
+        }
     }
 
-
+    @IBAction func playButtonTapped(_ sender: Any) {
+        let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
+        let url = URL(string: urlString)
+        let player = AVPlayer(url: url!)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+    
 }
 
