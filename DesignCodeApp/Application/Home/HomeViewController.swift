64ba77aa8e4b64e42366c30213424a01b879c1f6
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-02-27 21:31:49 -0400

    Adding 3d Animation using scroll

--- DesignCodeApp/Application/Home/HomeViewController.swift
+++ DesignCodeApp/Application/Home/HomeViewController.swift
@@ -25,6 +25,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
                 
         scrollView.delegate = self
+        chapterCollectionView.delegate = self
         chapterCollectionView.dataSource = self
         
         titleLabel.alpha = 0
@@ -62,10 +63,44 @@ extension HomeViewController: UIScrollViewDelegate {
             deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY / 4)
             backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY / 5)
         }
+        
+        if let collectionView = scrollView as? UICollectionView {
+            for cell in collectionView.visibleCells as! [ChapterCollectionViewCell] {
+                let indexPath = collectionView.indexPath(for: cell)!
+                let attributes = collectionView.layoutAttributesForItem(at: indexPath)!
+                let cellFrame = collectionView.convert(attributes.frame, to: view)
+                let translationX = cellFrame.origin.x / 5
+                cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
+                
+                cell.layer.transform = animateCell(cellFrame: cellFrame)
+            }
+        }
     }
+    
+    func animateCell(cellFrame: CGRect) -> CATransform3D {
+        let angleFromX = Double((-cellFrame.origin.x) / 10)
+        let angle = CGFloat((angleFromX * Double.pi) / 180.0)
+        var transform = CATransform3DIdentity
+        transform.m34 = -1.0/1000
+        let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+        
+        var scaleFromX = (1000 - (cellFrame.origin.x - 200)) / 1000
+        let scaleMax: CGFloat = 1.0
+        let scaleMin: CGFloat = 0.6
+        if scaleFromX > scaleMax {
+            scaleFromX = scaleMax
+        }
+        if scaleFromX < scaleMin {
+            scaleFromX = scaleMin
+        }
+        let scale = CATransform3DScale(CATransform3DIdentity, scaleFromX, scaleFromX, 1)
+        
+        return CATransform3DConcat(rotation, scale)
+    }
+
 }
 
-extension HomeViewController: UICollectionViewDataSource {
+extension HomeViewController: UICollectionViewDelegate ,UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return chaptersViewModel.numberOfItems
     }
@@ -74,6 +109,7 @@ extension HomeViewController: UICollectionViewDataSource {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ChapterCollectionViewCell
         cell.viewModel = chaptersViewModel.item(indexPath: indexPath)
+        cell.layer.transform = animateCell(cellFrame: cell.frame)
         return cell
     }
 }
